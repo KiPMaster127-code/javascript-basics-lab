69599d4ef96f73a87b5fcf131a5f09a1a7cdd9f4
KiPMaster127-code <misha.azaranka@gmail.com>
2025-09-21 18:03:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let studentName = "Mikhail";
+let studentAge = 21;
+let isEnrolled = true;
+
+console.log("Name:", studentName);
+console.log("Age:", studentAge);
+console.log("Enrolled:", isEnrolled);
+
+let myString = "JavaScript";
+let myNumber = 42;
+let myBoolean = false;
+let myArray = ["apple", "banana", "cherry"];
+let myObject = { firstName: "John", lastName: "Doe" };
+
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Boolean:", myBoolean);
+console.log("Array:", myArray);
+console.log("Object:", myObject);
+
+
+console.log("Add:", 5 + 3);
+console.log("Subtract:", 10 - 4);
+console.log("Multiply:", 6 * 2);
+console.log("Divide:", 20 / 5);
+
+
+console.log("Greater than:", 7 > 5);
+console.log("Less than:", 3 < 8);
+console.log("Equal to:", 10 === 10);
+console.log("Not equal:", 9 !== 5);
+
+
+console.log("AND:", true && false);
+console.log("OR:", true || false);
+console.log("NOT:", !true);
