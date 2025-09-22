a42fc9ecc4c513d2e65793b60f3dd425f2a8cea3
KiPMaster127-code <misha.azaranka@gmail.com>
2025-09-21 20:57:05 -0400

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let studentName = "Mikhail";
 let studentAge = 24;
 let isEnrolled = true;
 
-console.log("Name:", studentName);
-console.log("Age:", studentAge);
-console.log("Enrolled:", isEnrolled);
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
 
 let myString = "JavaScript";
 let myNumber = 42;
@@ -12,26 +12,27 @@ let myBoolean = false;
 let myArray = ["apple", "banana", "cherry"];
 let myObject = { firstName: "John", lastName: "Doe" };
 
-console.log("String:", myString);
-console.log("Number:", myNumber);
-console.log("Boolean:", myBoolean);
-console.log("Array:", myArray);
-console.log("Object:", myObject);
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myArray);
+console.log(myObject);
 
 
-console.log("Add:", 5 + 3);
-console.log("Subtract:", 10 - 4);
-console.log("Multiply:", 6 * 2);
-console.log("Divide:", 20 / 5);
+console.log(5 + 3);
+console.log(10 - 4);
+console.log(6 * 2);
+console.log(20 / 5);
 
 
-console.log("Greater than:", 7 > 5);
-console.log("Less than:", 3 < 8);
-console.log("Equal to:", 10 === 10);
-console.log("Not equal:", 9 !== 5);
+console.log(7 > 5);
+console.log(3 < 8);
+console.log(10 === 10);
+console.log(9 !== 5);
 
 
-console.log("AND:", true && false);
-console.log("OR:", true || false);
-console.log("NOT:", !true);
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
+
 
